d8a84a6112feb68fe60cd37e25afdecf4c38160b
Joe Lai <jlai@carnegiescience.edu>
2026-08-06 12:26:43 -0400

    feat: add control software, GUI components, and calibration data for laser heating experiments

---
+++ main.bat
@@ -0,0 +1,9 @@
+@echo off
+start pythonw "D:\Softwares\Laser-Heating-main_20250707\main.py"
+start pythonw "D:\Softwares\Laser-Heating-main_20250411\festo_control.py"
+start pythonw "D:\Softwares\20240611 Laser Heating\Laser Heat Control Current\main.py"
+start "" "C:\Users\LabUserR129\AppData\Local\Programs\Python\Python310\python.exe" "C:\Pyhon310Venv\venvfolder\simple_pyspin-master\examples\FLIR_4_Cam_Control_4_chatGPT_20260427_AllGreat.py"
+exit
+
+
+
